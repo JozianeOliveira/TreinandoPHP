907aee008593e7b2a07956df064f0a7d89c88833
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-10-18 08:22:54 -0300

    Refatora entrada do formulário e formatação da saída para melhor usabilidade e tratamento de valor padrão
    - Ajustei o campo de entrada para aceitar apenas números, alterando o tipo de input de `text` para `number`.
    - Corrigi o uso da variável `$numero` para definir um valor padrão (`0`) quando nenhum número for fornecido.
    - Ajustei a formatação da saída, usando `<em>` para estilizar "antecessor" e "sucessor".
    - Substituí o link "Voltar" por um botão com ícone para melhorar a usabilidade.

--- 03AntecessorSucessor/cad.php
+++ 03AntecessorSucessor/cad.php
@@ -11,15 +11,17 @@
         <h1>Resultado Final</h1>
     </header>
     <main>
+        <p>
         <?php 
-        $numero = $_GET["numero"];
+        $numero = $_GET["numero"] ?? 0;
         echo "<p>O número escolhido foi <strong>$numero</strong></p>";
         $antecessor = $numero - 1;
-        echo "<p>O seu antecessor é $antecessor</p>";
+        echo "<p>O seu <em>antecessor</em> é $antecessor</p>";
         $sucessor = $numero + 1;
-        echo "<p>O seu sucessor é $sucessor</p>";
+        echo "<p>O seu <em>sucessor</em> é $sucessor</p>";
         ?>
-        <p><a href="javascript:history.go(-1)">Voltar</a></p>
+        </p>
+        <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
     </main>
 </body>
 </html>
