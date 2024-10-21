512c8479d5cd6c8d5f76b1ceacee763f4aa87c95
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-10-21 09:44:43 -0300

    Criação de calculadora de salário para comparação com salário mínimo
    - Adicionado formulário em PHP para capturar o valor do salário inserido pelo usuário.
    - Implementada a funcionalidade para calcular quantos salários mínimos o usuário recebe com base em um valor de salário mínimo predefinido.
    - Utilizada a função intdiv() para calcular a parte inteira (quociente) da divisão do salário.
    - Calculado o resto da divisão para exibir o valor que excede os salários mínimos completos.
    - Formatados os valores de salário e do restante usando number_format() para exibição adequada em formato monetário.
    - Adicionada validação para garantir que o salário inserido seja maior que zero.

---
+++ 10SalarioMinimo/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salário Mínimo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php 
+    // Capturando os dados do Formulário Retroalimentado
+    $salario = $_GET['sal'] ?? 0;
+    $salmin = 1_412.00;
+    ?>
+
+    <main>
+        <h1>Informe seu salário</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="sal">Salário (R$)</label>
+            <input type="number" name="sal" id="sal" value="<?=$salario?>" step="0.01">
+            <p>Considerando o salário mínimo de <strong>R$<?=number_format($salmin, 2, ",", ".")?></strong></p>
+            <input type="submit" value="Calcular">
+        </form>
+
+        <section id="resultado">
+            <h2>Resultado Final</h2>
+            <?php 
+            $qtdde = intdiv($salario, $salmin);
+            $resto = $salario % $salmin;
+            echo "<p>Quem recebe um salário de R\$ " . number_format($salario, 2, ",", ".") . " ganha <strong>$qtdde salários mínimos</strong> + R\$ " . number_format($resto, 2, ",", ".") . "</p>"; 
+            ?>
+        </section>
+</body>
+</html>
