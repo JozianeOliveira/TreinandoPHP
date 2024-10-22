aa12206e14d6d3967966194577ead2b62a123356
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-10-22 11:00:21 -0300

    Renomeando os campos title do index.php

--- 15CalculadoraTempo/index.php
+++ 15CalculadoraTempo/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Raizes Quadradas e Cúbicas</title>
+    <title>Calculadora de Tempo</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
--- 16CaixaEletrônico/index.php
+++ 16CaixaEletrônico/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Raizes Quadradas e Cúbicas</title>
+    <title>Caixa Eletrônico</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
